971840b98df95043cd991ece4b60f7e6283e1ee1
Dmitrii <dmitryc174@gmail.com>
2023-04-09 14:34:21 +0400

    added switch

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
-<?php
-phpinfo();
+
+<html>
+<head>
+    <title>random-dices</title>
+</head>
+<body>
+
+<br>
+<form action="/roll.php" method="post">
+    <label for="qty">Dices: </label><input type="text" name="qty">
+    <br>
+    <select name = "dice">
+        <option value="4">d4</option>
+        <option value="6">d6</option>
+        <option value="8">d8</option>
+        <option value="10">d10</option>
+        <option value="12">d12</option>
+        <option value="20">d20</option>
+    </select>
+    <br>
+    <input type="submit" value="Roll!">
+</form>
+</body>
+</html>
--- roll.php
+++ roll.php
@@ -1,1 +1,27 @@
 <?php
+$post_qty = $_POST['qty'];
+$post_dice = $_POST['dice'];
+
+switch ($post_dice){
+    case 4:
+        $dice = 4;
+        break;
+    case 6:
+        $dice = 6;
+        break;
+    case 8:
+        $dice = 8;
+        break;
+    case 10:
+        $dice = 10;
+        break;
+    case 12:
+        $dice = 12;
+        break;
+    case 20:
+        $dice = 20;
+        break;
+    default:
+        return "Choose a dice";
+}
+
